d94ad4c9dd021de3b4842f0f9f9cddaf2205fd55
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-10-30 23:00:53 +0530

    Practical 2

--- que2.c
+++ que2.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <conio.h>
-
 int main()
 {
-    float P, T, I, SI;
-    printf("Enter the principal: ");
-    scanf("%f \n", &P);
-    printf("Enter the time taken :");
-    scanf("%f \n", &T);
-    printf("Enter the Intrest :");
-    scanf("%f \n", &I);
-    SI = (P * T * I) / 100;
-    printf("SI=%f", SI);
-    getch();
+    float a, b, c;
+    printf("Enter a :");
+    scanf("%f", &a);
+    printf("Enter b :");
+    scanf("%f", &b);
+    printf("Enter c :");
+    scanf("%f", &c);
+    if (a > b)
+    {
+        if (a > c)
+        {
+            printf("a is the greatest number");
+        }
+    }
+    else if (b > a && b > c)
+    {
+        printf("b is greatest number");
+    }
+    else
+    {
+        printf("c is the greatest number");
+    }
+    return 0;
 }
