0fd1179c97a1c971ad492364141aa4015c9cd03d
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-11-01 22:33:40 +0530

    Practical 2

--- que6.c
+++ que6.c
@@ -2,16 +2,26 @@
 #include <conio.h>
 int main()
 {
-    int age;
-    printf("Enter the age :");
-    scanf("%d", &age);
-    if (age >= 18)
+    float a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter cofficients a,b and c :");
+    scanf("%f %f %f", &a, &b, &c);
+    discriminant = b * b - 4 * a * c;
+    if (discriminant > 0)
     {
-        printf("Eligible to vote ");
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b + sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
+    }
+    else if (discriminant == 0)
+    {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %f", root1);
     }
     else
     {
-        printf("Not eligible to vote");
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        printf("root1 = %.2f + %.2f and root2 =%.2f-%.2f", realPart, imagPart, realPart, imagPart);
     }
     return 0;
 }
