19a9543908d45d54a63a8b5e4b427c745e81075c
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-11-04 22:32:23 +0530

    Practical 3

--- que3.c
+++ que3.c
@@ -2,16 +2,51 @@
 #include <conio.h>
 int main()
 {
-    int a;
-    printf("Enter the number a :");
-    scanf("%d", &a);
-    if (a % 2 == 0)
+    int month;
+    printf("Enter the month number :");
+    scanf("%d", &month);
+
+    switch (month)
     {
-        printf("The given number is even");
-    }
-    else
-    {
-        printf("The given number is odd");
+    case 1:
+        printf("Jan");
+        break;
+    case 2:
+        printf("Feb");
+        break;
+    case 3:
+        printf("Mar");
+        break;
+    case 4:
+        printf("April");
+        break;
+    case 5:
+        printf("May");
+        break;
+    case 6:
+        printf("June");
+        break;
+    case 7:
+        printf("July");
+        break;
+    case 8:
+        printf("August");
+        break;
+    case 9:
+        printf("September");
+        break;
+    case 10:
+        printf("Oct");
+        break;
+    case 11:
+        printf("November");
+        break;
+    case 12:
+        printf("December");
+        break;
+    default:
+        printf("Invalid month");
+        break;
     }
     return 0;
 }
