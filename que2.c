0b04f4ec0e8a4c0a8ea52a77580a454ef50391be
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-11-05 21:18:43 +0530

    Practical 4

--- que2.c
+++ que2.c
@@ -2,29 +2,14 @@
 #include <conio.h>
 int main()
 {
-    int color;
-    printf("Enter the number from 1 to 5 :");
-    scanf("%d", &color);
-    switch (color)
+    int n, i;
+    int sum = 0;
+    printf("Enter the number :");
+    scanf("%d", &n);
+    for (i = 0; i <= n; i++)
     {
-    case 1:
-        printf("RED");
-        break;
-    case 2:
-        printf("GREEN");
-        break;
-    case 3:
-        printf("WHITE");
-        break;
-    case 4:
-        printf("YELLOW");
-        break;
-    case 5:
-        printf("ORANGE");
-        break;
-
-    default:
-        printf("Invalid input");
-        break;
+        sum = sum + i;
     }
+    printf("sum=%d", sum);
+    return 0;
 }
