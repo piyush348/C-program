7ebe6b100069fca98206b42ae99455b8c03f0db1
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-11-05 21:18:14 +0530

    Practical 4

--- que1.c
+++ que1.c
@@ -2,35 +2,12 @@
 #include <conio.h>
 int main()
 {
-    int a, b, c;
-    char operator;
-    printf("Enter any one operator :");
-    scanf("%c", &operator);
-    printf("Enter the value a :");
-    scanf("%d", &a);
-    printf("Enter the value b :");
-    scanf("%d", &b);
-    switch (operator)
+    int N, i;
+    printf("Enter the number:");
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++)
     {
-    case '+':
-        c = a + b;
-        printf("The sum is %d", c);
-        break;
-    case '-':
-        c = a - b;
-        printf("The difference is %d", c);
-        break;
-    case '*':
-        c = a * b;
-        printf("The product is %d", c);
-        break;
-    case '/':
-        c = a / b;
-        printf("The quotient is %d", c);
-        break;
-
-    default:
-        printf("Invalid entry");
-        break;
+        printf("%d\n", i);
     }
+    return 0;
 }
