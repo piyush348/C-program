eb6e327215071ca306e4d6115062bc4884f7f39b
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-11-06 22:29:09 +0530

    Practical 3

--- que3.c
+++ que3.c
@@ -2,51 +2,14 @@
 #include <conio.h>
 int main()
 {
-    int month;
-    printf("Enter the month number :");
-    scanf("%d", &month);
-
-    switch (month)
+    int i, n;
+    int fact = 1;
+    printf("Enter the number :");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-    case 1:
-        printf("Jan");
-        break;
-    case 2:
-        printf("Feb");
-        break;
-    case 3:
-        printf("Mar");
-        break;
-    case 4:
-        printf("April");
-        break;
-    case 5:
-        printf("May");
-        break;
-    case 6:
-        printf("June");
-        break;
-    case 7:
-        printf("July");
-        break;
-    case 8:
-        printf("August");
-        break;
-    case 9:
-        printf("September");
-        break;
-    case 10:
-        printf("Oct");
-        break;
-    case 11:
-        printf("November");
-        break;
-    case 12:
-        printf("December");
-        break;
-    default:
-        printf("Invalid month");
-        break;
+        fact = fact * i;
     }
+    printf("factorial=%d", fact);
     return 0;
 }
