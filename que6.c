9707563c2bf4d75d22fdd0a08aa609bf062b312b
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-11-07 22:11:07 +0530

    Practical 4

--- que6.c
+++ que6.c
@@ -2,26 +2,22 @@
 #include <conio.h>
 int main()
 {
-    float a, b, c, discriminant, root1, root2, realPart, imagPart;
-    printf("Enter cofficients a,b and c :");
-    scanf("%f %f %f", &a, &b, &c);
-    discriminant = b * b - 4 * a * c;
-    if (discriminant > 0)
+    int i, n;
+    int count = 0;
+    printf("Enter the value of n :");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b + sqrt(discriminant)) / (2 * a);
-        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
+        if (n % i == 0)
+            count++;
     }
-    else if (discriminant == 0)
+    if (count == 2)
     {
-        root1 = root2 = -b / (2 * a);
-        printf("root1 = root2 = %f", root1);
+        printf("Is prime number");
     }
     else
     {
-        realPart = -b / (2 * a);
-        imagPart = sqrt(-discriminant) / (2 * a);
-        printf("root1 = %.2f + %.2f and root2 =%.2f-%.2f", realPart, imagPart, realPart, imagPart);
+        printf("Not a primr number");
     }
     return 0;
 }
