a26b9a240d5d048c82c8e13f7d850ac4c7f299e6
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-12-20 00:05:11 +0530

    Practical 6

--- que2.c
+++ que2.c
@@ -1,15 +1,57 @@
+// C program to add and multiply two compatible matrices
 #include <stdio.h>
 #include <conio.h>
 int main()
 {
-    int n, i;
-    int sum = 0;
-    printf("Enter the number :");
-    scanf("%d", &n);
-    for (i = 0; i <= n; i++)
+    int a[3][3], b[3][3], c[3][3] = {0}, d[3][3] = {0};
+    int i, j, k, m, n, p, q;
+    printf("Enter no. of rows and columns in matrix A: ");
+    scanf("%d%d", &m, &n);
+    printf("Enter no. of rows and columns in matrix B: ");
+    scanf("%d%d", &p, &q);
+    if (m != p || n != q)
     {
-        sum = sum + i;
+        printf("Matrix Addition is not possible");
+        return;
     }
-    printf("sum=%d", sum);
-    return 0;
+    else if (n != p)
+    {
+        printf("Matrix Multiplication is not possible");
+        return;
+    }
+    else
+    {
+        printf("Enter elements of matrix A: ");
+        for (i = 0; i < m; i++)
+            for (j = 0; j < n; j++)
+                scanf("%d", &a[i][j]);
+        printf("Enter elements of matrix B: ");
+        for (i = 0; i < p; i++)
+            for (j = 0; j < q; j++)
+                scanf("%d", &b[i][j]);
+        // Matrix Addition
+        for (i = 0; i < m; i++)
+            for (j = 0; j < n; j++)
+                c[i][j] = a[i][j] + b[i][j];
+        printf("\nResult of Matirx Addition:\n");
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+                printf("%d ", c[i][j]);
+            printf("\n");
+        }
+        // Matrix Multiplication
+        for (i = 0; i < m; i++)
+            for (j = 0; j < q; j++)
+                for (k = 0; k < p; k++)
+                    d[i][j] += a[i][k] * b[k][j];
+        printf("\nResult of Matirx Multiplication:\n");
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < q; j++)
+                printf("%d ", d[i][j]);
+            printf("\n");
+        }
+    }
+    getch();
 }
