ec470ebf8df2493c64944f6f7a38889ec536f5f4
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-12-21 21:53:52 +0530

    Practical 6

--- que3.c
+++ que3.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
-#include <conio.h>
+
 int main()
 {
-    int i, n;
-    int fact = 1;
-    printf("Enter the number :");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
+    int mat[12][12];
+    int i, j, row, col, sum = 0;
+    printf("Enter the number of rows and columns for 1st matrix\n");
+    scanf("%d%d", &row, &col);
+    printf("Enter the elements of the matrix\n");
+    for (i = 0; i < row; i++)
     {
-        fact = fact * i;
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
     }
-    printf("factorial=%d", fact);
+
+    printf("The matrix\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+    // To add diagonal elements
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (i == j)
+            {
+                sum = sum + mat[i][j];
+            }
+        }
+    }
+
+    printf("The sum of diagonal elements of a square matrix = %d\n", sum);
+
     return 0;
 }
