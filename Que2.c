20c2d138320caa207598c2adecef8050d0dae3f9
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2023-12-25 22:37:39 +0530

    Practical 7

--- Que2.c
+++ Que2.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
+#include <conio.h>
+
+
+int CheckPrime(int number)
+{
+    
+    int i = 2;
+    while (i <= number / 2)
+    {
+        if (number % i == 0)
+            return 0;
+        else
+            i++;
+    }
+    return 1;
+}
 int main()
 {
-    int arr1[5], arr2[5], sum[5], i;
-    printf("Enter first array : \n");
-    for (i = 0; i <= 5; i++)
-    {
-        printf("arr1[%d]=", i);
-        scanf("%d", &arr1[i]);
-    }
-    printf("Enter second array : \n");
-    for (i = 0; i <= 5; i++)
-    {
-        printf("arr2[%d]=", i);
-        scanf("%d", &arr2[i]);
-    }
-    for (i = 0; i <= 5; i++)
-    {
-        sum[i] = arr1[i] + arr2[i];
-    }
-    printf("Sum of array :");
-    for (i = 0; i < 5; i++)
-    {
-        printf("\nsum[%d]=%d", i, sum[i]);
-    }
+    int num, prime;
+    printf("Enter the number :");
+    scanf("%d", &num);
+
+    prime = CheckPrime(num);
+
+    if (prime == 1)
+        printf("%d is a prime number", num);
+    else
+        printf("%d is not a prime number", num);
     return 0;
 }
